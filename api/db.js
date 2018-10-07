3b61ea4ccc0f0ad252a916d0d0aaff05d3f1c2ca
Adrian Darian <adarian@ucmerced.edu>
2018-10-06 17:08:28 -0700

    idk what I am doing

--- api/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const {dbUsername, dbPassword} = require('./secrets');
-const url = 'mongodb+srv://' + dbUsername + ':' + dbPassword + '@projects-dy3dy.gcp.mongodb.net';
-
-module.exports.main = mongoose.createConnection(url + '/main', {useNewUrlParser: true});
